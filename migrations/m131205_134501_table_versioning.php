73f21f914845e26d5344d68b6a02480dcb2341a3
Mark <m@rkw.me>
2013-12-12 09:57:22 +0000

    [OE-3856] consolidated migrations

--- migrations/m131205_134501_table_versioning.php
+++ migrations/m131205_134501_table_versioning.php
@@ -6,20 +6,20 @@ class m131205_134501_table_versioning extends CDbMigration
 	{
 		$this->execute("
 CREATE TABLE `et_ophimultrasound_report_version` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `event_id` int(10) unsigned NOT NULL,
-  `report` text COLLATE utf8_bin,
-  `last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  PRIMARY KEY (`id`),
-  KEY `acv_et_ophimultrasound_report_lmui_fk` (`last_modified_user_id`),
-  KEY `acv_et_ophimultrasound_report_cui_fk` (`created_user_id`),
-  KEY `acv_et_ophimultrasound_report_ev_fk` (`event_id`),
-  CONSTRAINT `acv_et_ophimultrasound_report_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophimultrasound_report_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophimultrasound_report_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`event_id` int(10) unsigned NOT NULL,
+	`report` text COLLATE utf8_bin,
+	`last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	PRIMARY KEY (`id`),
+	KEY `acv_et_ophimultrasound_report_lmui_fk` (`last_modified_user_id`),
+	KEY `acv_et_ophimultrasound_report_cui_fk` (`created_user_id`),
+	KEY `acv_et_ophimultrasound_report_ev_fk` (`event_id`),
+	CONSTRAINT `acv_et_ophimultrasound_report_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophimultrasound_report_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophimultrasound_report_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 		");
 
@@ -37,23 +37,23 @@ CREATE TABLE `et_ophimultrasound_report_version` (
 
 		$this->execute("
 CREATE TABLE `et_ophimultrasound_request_version` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `event_id` int(10) unsigned NOT NULL,
-  `priority_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `indication` text COLLATE utf8_bin,
-  `last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  PRIMARY KEY (`id`),
-  KEY `acv_et_ophimultrasound_request_lmui_fk` (`last_modified_user_id`),
-  KEY `acv_et_ophimultrasound_request_cui_fk` (`created_user_id`),
-  KEY `acv_et_ophimultrasound_request_ev_fk` (`event_id`),
-  KEY `acv_ophimultrasound_request_priority_fk` (`priority_id`),
-  CONSTRAINT `acv_et_ophimultrasound_request_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophimultrasound_request_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophimultrasound_request_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`),
-  CONSTRAINT `acv_ophimultrasound_request_priority_fk` FOREIGN KEY (`priority_id`) REFERENCES `ophimultrasound_request_priority` (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`event_id` int(10) unsigned NOT NULL,
+	`priority_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`indication` text COLLATE utf8_bin,
+	`last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	PRIMARY KEY (`id`),
+	KEY `acv_et_ophimultrasound_request_lmui_fk` (`last_modified_user_id`),
+	KEY `acv_et_ophimultrasound_request_cui_fk` (`created_user_id`),
+	KEY `acv_et_ophimultrasound_request_ev_fk` (`event_id`),
+	KEY `acv_ophimultrasound_request_priority_fk` (`priority_id`),
+	CONSTRAINT `acv_et_ophimultrasound_request_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophimultrasound_request_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophimultrasound_request_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`),
+	CONSTRAINT `acv_ophimultrasound_request_priority_fk` FOREIGN KEY (`priority_id`) REFERENCES `ophimultrasound_request_priority` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 		");
 
@@ -71,18 +71,18 @@ CREATE TABLE `et_ophimultrasound_request_version` (
 
 		$this->execute("
 CREATE TABLE `ophimultrasound_request_priority_version` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) COLLATE utf8_bin NOT NULL,
-  `display_order` int(10) unsigned NOT NULL DEFAULT '1',
-  `last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  PRIMARY KEY (`id`),
-  KEY `acv_ophimultrasound_request_priority_lmui_fk` (`last_modified_user_id`),
-  KEY `acv_ophimultrasound_request_priority_cui_fk` (`created_user_id`),
-  CONSTRAINT `acv_ophimultrasound_request_priority_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_ophimultrasound_request_priority_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`name` varchar(128) COLLATE utf8_bin NOT NULL,
+	`display_order` int(10) unsigned NOT NULL DEFAULT '1',
+	`last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	PRIMARY KEY (`id`),
+	KEY `acv_ophimultrasound_request_priority_lmui_fk` (`last_modified_user_id`),
+	KEY `acv_ophimultrasound_request_priority_cui_fk` (`created_user_id`),
+	CONSTRAINT `acv_ophimultrasound_request_priority_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_ophimultrasound_request_priority_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 		");
 
@@ -97,10 +97,26 @@ CREATE TABLE `ophimultrasound_request_priority_version` (
 		$this->addColumn('ophimultrasound_request_priority_version','version_id','int(10) unsigned NOT NULL');
 		$this->addPrimaryKey('version_id','ophimultrasound_request_priority_version','version_id');
 		$this->alterColumn('ophimultrasound_request_priority_version','version_id','int(10) unsigned NOT NULL AUTO_INCREMENT');
+
+		$this->addColumn('et_ophimultrasound_report','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('et_ophimultrasound_report_version','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('et_ophimultrasound_request','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('et_ophimultrasound_request_version','deleted','tinyint(1) unsigned not null');
+
+		$this->addColumn('ophimultrasound_request_priority','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('ophimultrasound_request_priority_version','deleted','tinyint(1) unsigned not null');
 	}
 
 	public function down()
 	{
+		$this->dropColumn('ophimultrasound_request_priority','deleted');
+		$this->dropColumn('ophimultrasound_request_priority_version','deleted');
+
+		$this->dropColumn('et_ophimultrasound_report','deleted');
+		$this->dropColumn('et_ophimultrasound_report_version','deleted');
+		$this->dropColumn('et_ophimultrasound_request','deleted');
+		$this->dropColumn('et_ophimultrasound_request_version','deleted');
+
 		$this->dropTable('et_ophimultrasound_report_version');
 		$this->dropTable('et_ophimultrasound_request_version');
 		$this->dropTable('ophimultrasound_request_priority_version');
